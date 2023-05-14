2b4ee6a33ae096bd7c059a4f2f3190b159a9af0b
Josh Davis <joshdavis97@icloud.com>
2023-05-14 11:17:39 +0100

    Added basic loading page features

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,12 @@ fetch(`cocktaildb_api_clone_local.txt`)
 )
 .then(() => {
     console.log(allDrinks);
-    //code here to switch from loading screen to display data
+    // code here to switch from loading screen to display data
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+        document.querySelector("main").style.display = "flex";
+        document.querySelector("footer").style.display = "flex";
+        document.querySelector("#index-page").style.display = "flex";
+    },1000);
 })    
 .catch((error) => console.log(error));
