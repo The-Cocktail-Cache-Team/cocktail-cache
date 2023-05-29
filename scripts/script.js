f6b43dfb15c95a233c0e169d10d9e52fe7b05e9e
Josh Davis <joshdavis97@icloud.com>
2023-05-29 11:29:30 +0100

    Tidy and spell check JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-//API fetch code - using local api for now
-
+//Global Variables
 let allDrinks;
 let allDrinkNames;
 let allDrinkIngredients = [];
@@ -10,6 +9,7 @@ let complexityFilterValue = 16;
 let chosenOptions = [];
 let questionTracker = 0;
 
+//API Fetch and populate global variables with cocktail list - using local API for now
 fetch(`cocktaildb_api_clone_local.txt`)
 .then((result) => result.json())
 .then((json) => {
@@ -32,7 +32,7 @@ fetch(`cocktaildb_api_clone_local.txt`)
 
     allDrinkNamesAndIngredients = allDrinkNames.concat(allDrinkIngredients);
 
-    // sort array in alphabetcial order
+    // sort array in alphabetical order
     allDrinkNames.sort();
     allDrinkIngredients.sort();
     allDrinkNamesAndIngredients.sort();
@@ -42,6 +42,7 @@ fetch(`cocktaildb_api_clone_local.txt`)
         document.getElementById("loading-screen").style.display = "none";
         document.querySelector("main").style.display = "flex";
 
+        //runs additional function based on page to prevent site bugs
         if(window.location.pathname === "/browse.html") {
             addFilterBtnEvntListener();
             populateFilterBox();
@@ -49,12 +50,11 @@ fetch(`cocktaildb_api_clone_local.txt`)
         } else if(window.location.pathname === "/details.html") {
             displayCurrentCocktail();
         }
-
     },1000);
 })
 .catch((error) => console.log(error));
 
-// Displaying the images on the page
+// Renders cards html based on allDrinksToDisplay array
 const displayCocktails = () =>{
     const cocktailsContainer = document.getElementById("cocktail-container");
     const errorDisplay = document.getElementById("no-results");
@@ -76,9 +76,7 @@ const displayCocktails = () =>{
    }
 }
 
-// Details Page - Displaying the images
-
-
+// Renders content for details page based on ID in URL
 function displayCurrentCocktail () {
     const urlParams = new URLSearchParams(window.location.search);
     const currentId = urlParams.get("id");
@@ -94,10 +92,10 @@ function displayCurrentCocktail () {
         }
     }
 
-    const ingredientMeasurments = [];
+    const ingredientMeasurements = [];
     for (let i = 1; i <= currentIngredients.length; i++) {
         const objKey = "strMeasure" + i;
-        ingredientMeasurments.push(currentDrink[objKey]);
+        ingredientMeasurements.push(currentDrink[objKey]);
     }
 
     const cocktailImgSrc = currentDrink.strDrinkThumb;
@@ -105,8 +103,8 @@ function displayCurrentCocktail () {
     const cocktailTitle = currentDrink.strDrink;
     const cocktailIngredients = currentIngredients
     .map((item, index) => {
-        if(ingredientMeasurments[index] !== null) {
-            return `<button onclick="search('${item}')">${ingredientMeasurments[index].toUpperCase()} ${item}</button>`;
+        if(ingredientMeasurements[index] !== null) {
+            return `<button onclick="search('${item}')">${ingredientMeasurements[index].toUpperCase()} ${item}</button>`;
         } else {
             return `<button onclick="search('${item}')">${item}</button>`;
         }
@@ -123,7 +121,7 @@ function displayCurrentCocktail () {
     document.getElementById("serve-glass").innerHTML = cocktailServe;
     };
 
-// FILTER RESULTS FUNCTION
+// Filter button interactivity for browse page
 function addFilterBtnEvntListener() {
     const filterBtn = document.querySelector("#filter-btn");
     const arrow1 = document.getElementById("arrow1");
@@ -143,8 +141,9 @@ function addFilterBtnEvntListener() {
     })
 }
 
+//Renders buttons for the filter box on the browse page
 function populateFilterBox() {
-    const ingredientsBox = document.getElementById("ingreditents");
+    const ingredientsBox = document.getElementById("ingredients");
 
     const allIngredients = allDrinkIngredients.map(item => `<div class="ingredient-container" id="${item}" onclick="toggleFilter('${item}')"><p>${item}</p></div>`);
 
@@ -168,6 +167,7 @@ function populateFilterBox() {
     })
 }
 
+// add or removes ingredient filter when clicking an ingredient
 function toggleFilter (ingredient) {
     if(!filters.includes(ingredient)){
         filters.push(ingredient);
@@ -185,10 +185,12 @@ function toggleFilter (ingredient) {
     updateDrinksToDisplay();
 }
 
+//updates the allDrinksToDisplay array with objects that fit all filter criteria
 function updateDrinksToDisplay () {
+    //Start with all drinks
     allDrinksToDisplay = allDrinks;
 
-    //FILTER FOR INGREDIENTS
+    //Loop through array filtering out each chosen filter
     if(filters.length !== 0){
         filters.forEach(ingredientName => {
             const newArr = allDrinksToDisplay.filter(cocktailObj => {
@@ -204,7 +206,7 @@ function updateDrinksToDisplay () {
         });
     }
 
-    //FILTER FOR COMPLEXITY
+    //Loop through array filtering out each maximum ingredient input
     if(complexityFilterValue !== 16){
         const filteredForComplexity = allDrinksToDisplay.filter(cocktailObj => {
             let ingredientCounter = 0;
@@ -223,7 +225,7 @@ function updateDrinksToDisplay () {
         allDrinksToDisplay = filteredForComplexity;
     }
 
-    //FILTER FOR ALCOHOL
+    //Loop through array filtering out chosen alcoholic input
     const containsAlcoholRadio = document.getElementById("contains-alcohol");
     const noContainsAlcoholRadio = document.getElementById("no-contains-alcohol");
 
@@ -238,7 +240,7 @@ function updateDrinksToDisplay () {
     displayCocktails();
 }
 
-
+//Checks if URL has pre-selected filter and adds filter before loading cards
 function preFilterCheck () {
     const currentId = new URLSearchParams(window.location.search).get("id");
     if(currentId !== null){
@@ -248,6 +250,7 @@ function preFilterCheck () {
     }
 };
 
+//Interactive number update with max ingredient slider
 function checkComplexityFilter () {
     const complexitySlider = document.getElementById("complexity-slider");
     const complexityMessage = document.getElementById("complexity-message");
@@ -262,7 +265,7 @@ function checkComplexityFilter () {
     }
 }
 
-///////NAV BAR SEARCH FEATURES///////
+//Navigation Bar - Input & Predictive Search 
 const searchInput = document.getElementById("nav-search-input");
 const searchBtn = document.getElementById("nav-search-btn");
 const predictiveSearchContainer = document.getElementById("predictive-search-results");
@@ -294,6 +297,7 @@ searchInput.oninput = () =>{
     }
 };
 
+//Navigation bar - Search Button
 searchBtn.onclick = e => {
     e.preventDefault();
     if(allDrinkNamesAndIngredients.includes(searchInput.value.toUpperCase())) {
@@ -301,12 +305,13 @@ searchBtn.onclick = e => {
     }
 };
 
-const mainSearchInput = document.getElementById("main-search-input");
-const mainSearchBtn = document.getElementById("main-search-btn");
-const mainPredictiveSearchContainer = document.getElementById("main-predictive-search-results");
-
-
+//Runs code only for Index page to prevent site bugs
 if(window.location.pathname === "/index.html"){
+    const mainSearchInput = document.getElementById("main-search-input");
+    const mainSearchBtn = document.getElementById("main-search-btn");
+    const mainPredictiveSearchContainer = document.getElementById("main-predictive-search-results");
+
+    //Index Navigation Bar - Input & Predictive Search 
     mainSearchInput.oninput = () =>{
         if(mainSearchInput.value !== ""){
             mainPredictiveSearchContainer.style.display="block";
@@ -334,6 +339,7 @@ if(window.location.pathname === "/index.html"){
         }
     };
 
+    //Index Navigation bar - Search Button
     mainSearchBtn.onclick = e => {
         e.preventDefault();
         if(allDrinkNamesAndIngredients.includes(mainSearchInput.value.toUpperCase())) {
@@ -341,14 +347,16 @@ if(window.location.pathname === "/index.html"){
         }
     };
 
+    //Index Page Browse/ Quiz Big Buttons
     document.getElementById("browse-index-btn").addEventListener("click", () => {window.location.href = "./browse.html"})
     document.getElementById("quiz-index-btn").addEventListener("click", () => {window.location.href = "./quiz.html"})
 
+    //Index Page Big Random Button
     const randomIndexBtn = document.getElementById("random-index-btn");
     randomIndexBtn.addEventListener("click", showRandom);
 };
 
-
+//Navigate to drink or browse page with filter
 function search(searchString) {
     console.log(searchInput.value.toUpperCase());
     const isSearchingForCocktail = allDrinkNames.some(item => item === searchString);
@@ -361,8 +369,7 @@ function search(searchString) {
     }
 }
 
-//MOBILE MENU
-
+//Mobile Nav
 let mobileMenuOpen = false;
 
 const mobileMenuBtn = document.getElementById("mobile-menu-btn");
@@ -381,14 +388,13 @@ mobileMenuBtn.addEventListener("click", () => {
     }
 });
 
-// -- RANDOM COCKTAIL FEATURE
-
+// RANDOM COCKTAIL FEATURE
 const navLinkRandom = document.getElementById("random-cocktail-nav-btn");
 navLinkRandom.addEventListener("click", showRandom);
 
 const getRandomIndex = max => Math.floor(Math.random() * max);
 
-    // Fn to open details page using a random idDrink
+// Fn to open details page using a random idDrink
 function showRandom() {
     const randomInteger = getRandomIndex(allDrinks.length);
     const randomDrinkObject = allDrinks[randomInteger];
@@ -397,8 +403,7 @@ function showRandom() {
     window.location.href = `./details.html?id=${randomDrinkById}`;
 }
 
-// COCKTAIL QUIZ
-
+//Runs code only for quiz page to prevent site bugs
 if(window.location.pathname === "/quiz.html") {
     const quizTitle = document.querySelector("#quiz-container h1");
 
@@ -406,70 +411,73 @@ if(window.location.pathname === "/quiz.html") {
         quizTitle.classList.add("show");
       });
 
-    //store questions and options
-  const questions = [
-    {
-      question: "First things first - Do you want your drink to include alcohol?",
-      key: 'strAlcoholic',
-      options: ["Yes, booze me up please!", "No thanks, get me a mocktail!"]
-    },
-    {
-      question: "Pick your favorite glass to drink out of:",
-      key: 'strGlass',
-      options: ["Highball glass", "Coffee mug", "Shot glass", "Cocktail glass", "Mason Jar"]
-    },
-    {
-      question: "Do you prefer your drink recipe to have a shorter or longer list of ingredients?",
-      key: 'strIngredient',
-      options: ["The simpler, the better", "Give me something more complex"]
-    }
-  ];
+//store questions and options
+    const questions = [
+        {
+        question: "First things first - Do you want your drink to include alcohol?",
+        key: 'strAlcoholic',
+        options: ["Yes, booze me up please!", "No thanks, get me a mocktail!"]
+        },
+        {
+        question: "Pick your favorite glass to drink out of:",
+        key: 'strGlass',
+        options: ["Highball glass", "Coffee mug", "Shot glass", "Cocktail glass", "Mason Jar"]
+        },
+        {
+        question: "Do you prefer your drink recipe to have a shorter or longer list of ingredients?",
+        key: 'strIngredient',
+        options: ["The simpler, the better", "Give me something more complex"]
+        }
+    ];
 
-  const questionContainer = document.getElementById('question-container');
-  const options = document.getElementById('options-container');
-  const results = document.getElementById('result-container');
+    const questionContainer = document.getElementById('question-container');
+    const options = document.getElementById('options-container');
+    const results = document.getElementById('result-container');
 
-//Utility function to load questions and generate html elements using tracking variable
+    //Utility function to load questions and generate html elements using tracking variable
     function loadQuestion() {
-      questionContainer.innerHTML = ""; 
-      options.innerHTML = "";
-      results.innerHTML = "";
+        questionContainer.innerHTML = ""; 
+        options.innerHTML = "";
+        results.innerHTML = "";
     
-    const currentQuestion = questions[questionTracker];
-      const questionElement = document.createElement("p");
-      questionElement.textContent = `Q${questionTracker + 1}: ${currentQuestion.question}`;
-      questionContainer.appendChild(questionElement);
+        const currentQuestion = questions[questionTracker];
+        const questionElement = document.createElement("p");
+        questionElement.textContent = `Q${questionTracker + 1}: ${currentQuestion.question}`;
+        questionContainer.appendChild(questionElement);
     
-      currentQuestion.options.forEach((option) => {
-        const optionElement = document.createElement("button");
-        optionElement.textContent = option;
-        optionElement.addEventListener("click", handleOptionSelect);
-        options.appendChild(optionElement);
-      });
+        currentQuestion.options.forEach((option) => {
+            const optionElement = document.createElement("button");
+            optionElement.textContent = option;
+            optionElement.addEventListener("click", handleOptionSelect);
+            options.appendChild(optionElement);
+        });
     };
+
+    //Load first question on page load
     loadQuestion();
 
-    
+    //Stores answer and loads next question or finishes quiz
     function handleOptionSelect(event) {
         event.preventDefault();
-      const selectedOption = event.target.textContent;
-      chosenOptions.push(selectedOption);
-      questionTracker++;
-      if (chosenOptions.length === questions.length) {
-        calculateResult();
-      } else {
-        loadQuestion();
-        console.log("test");
-      }
+        const selectedOption = event.target.textContent;
+        chosenOptions.push(selectedOption);
+        questionTracker++;
+        if (chosenOptions.length === questions.length) {
+            calculateResult();
+        } else {
+            loadQuestion();
+        }
     };
 
+    //Finds cocktail match based on quiz answers
     function calculateResult() {
         document.addEventListener("DOMContentLoaded", function() {
             const title = document.querySelector("#quiz-container h1");
             title.classList.add("show");
-          });
-      // Filter drinks based on chosen options
-      let filteredDrinks = allDrinks.filter(drink => {
+        });
+
+        // Filter drinks based on chosen options
+        let filteredDrinks = allDrinks.filter(drink => {
         // Check first question: alcohol preference
         const alcoholOption = chosenOptions[0];
         let isAlcoholic = true;
@@ -482,71 +490,71 @@ if(window.location.pathname === "/quiz.html") {
         }});
 
 
-         // Check second question: glass preference
+        // Check second question: glass preference
         let filterDrinks2 = filteredDrinks.filter(drink => {
             if (drink.strGlass === chosenOptions[1]) {
                 return drink;
             }
-
-    });
+        });
     
         // Check third question: ingredients
         let filterDrinks3 = filterDrinks2.filter(drink => {
-          let ingredientCounter = 0;
-          for (let i = 1; i <= 15; i++) {
-            const objKey = "strIngredient" + i;
-            if (drink[objKey] !== null) {
-                ingredientCounter++;
-            }};
-            if (chosenOptions[2] === "The simpler, the better" && ingredientCounter <= 7) {
-                return drink;
-            } else if (chosenOptions[2] === "Give me something more complex" && ingredientCounter >= 7) {
-                return drink;
-            };
-    });
+            let ingredientCounter = 0;
+            for (let i = 1; i <= 15; i++) {
+                const objKey = "strIngredient" + i;
+                if (drink[objKey] !== null) {
+                    ingredientCounter++;
+                }};
+                if (chosenOptions[2] === "The simpler, the better" && ingredientCounter <= 7) {
+                    return drink;
+                } else if (chosenOptions[2] === "Give me something more complex" && ingredientCounter >= 7) {
+                    return drink;
+                };
+        });
 
-    let resultAccuracy = 100;
 
-    if (filterDrinks3.length < 1) {
-        filterDrinks3 = filterDrinks2;
-        resultAccuracy = 66;
+        //Checks there is a match, reduces filtering if not
+        let resultAccuracy = 100;
+
+        if (filterDrinks3.length < 1) {
+            filterDrinks3 = filterDrinks2;
+            resultAccuracy = 66;
+        };
+
+        if (filterDrinks2.length < 1) {
+            filterDrinks3 = filteredDrinks;
+            resultAccuracy = 33;
+        };
+    
+        const randomInteger = getRandomIndex(filterDrinks3.length);
+        const randomDrinkId = filterDrinks3[randomInteger].idDrink;
+
+        //present user with match %
+        quizTitle.style.display = "none";
+        questionContainer.style.display = "none";
+        options.style.display = "none";
+        results.style.display = "flex";
+
+        const resultMessage = document.createElement("h2");
+        resultMessage.innerHTML = `FOUND A ${resultAccuracy}% COCKTAIL MATCH`;
+        results.appendChild(resultMessage);
+
+        const resultLoadingBar = document.createElement("div");
+        resultLoadingBar.classList.add("loading-bar");
+
+        const resultLoadingBarInner = document.createElement("div");
+        resultLoadingBarInner.classList.add("loading-bar-inner");
+
+        resultLoadingBar.appendChild(resultLoadingBarInner);
+        results.appendChild(resultLoadingBar);
+
+    
+        setTimeout(() => {
+            resultLoadingBarInner.style.width = "calc(100% + 4px)";
+        }, 0)
+
+        setTimeout(() => {
+            window.location.href = `./details.html?id=${randomDrinkId}`;
+        }, 2500)
     };
-
-    if (filterDrinks2.length < 1) {
-        filterDrinks3 = filteredDrinks;
-        resultAccuracy = 33;
-    };
-    
-    const randomInterger = getRandomIndex(filterDrinks3.length);
-    const randomDrinkId = filterDrinks3[randomInterger].idDrink;
-
-    quizTitle.style.display = "none";
-    questionContainer.style.display = "none";
-    options.style.display = "none";
-    results.style.display = "flex";
-
-    const resultMessage = document.createElement("h2");
-    resultMessage.innerHTML = `FOUND A ${resultAccuracy}% COCKTAIL MATCH`;
-    results.appendChild(resultMessage);
-
-    const resultLoadingBar = document.createElement("div");
-    resultLoadingBar.classList.add("loading-bar");
-
-    const resultLoadingBarInner = document.createElement("div");
-    resultLoadingBarInner.classList.add("loading-bar-inner");
-
-    resultLoadingBar.appendChild(resultLoadingBarInner);
-    results.appendChild(resultLoadingBar);
-
-    
-    setTimeout(() => {
-        resultLoadingBarInner.style.width = "calc(100% + 4px)";
-    }, 0)
-
-    setTimeout(() => {
-        window.location.href = `./details.html?id=${randomDrinkId}`;
-    }, 2500)
-    
-}; 
-
 };
